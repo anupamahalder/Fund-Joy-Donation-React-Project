9fc8e873c5408d9d35a8031338243b4bab01f3b1
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-27 18:48:30 +0530

    Styled each card of donations

--- src/Pages/Home/DonationCard.jsx
+++ src/Pages/Home/DonationCard.jsx
@@ -4,8 +4,10 @@ const DonationCard = ({donation}) => {
     //destructuring donation ojbect
     const {ID, Picture, Title, Category, Category_bg, Card_bg, Text_button_bg} = donation;
     return (
-        <div className='rounded-xl h-60 p-2' style={{backgroundColor: Card_bg}}>
-            <img className='h-36 w-60 rounded-xl' src={Picture} alt="" />
+        <div className='rounded-xl h-80 p-2 mb-8' style={{backgroundColor: Card_bg}}>
+            <img className='h-40 w-64 rounded-xl mx-auto' src={Picture} alt="" />
+            <button className='ml-4 px-2 py-1 my-5 rounded-md font-semibold' style={{backgroundColor: Category_bg, color:Text_button_bg}}>{Category}</button>
+            <h1 className='pl-4 font-bold text-2xl' style={{color: Text_button_bg}}>{Title}</h1>
         </div>
     );
 };
